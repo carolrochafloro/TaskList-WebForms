fc72e4938437ed8dd65a85663d80fc5e73ab0169
Carolina Floro <carolinarochaf@hotmail.com>
2024-08-27 18:00:54 -0300

    Adicionar arquivos de projeto.

---
+++ WebFormsPractice/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebFormsPractice.Models
+{
+    public class Category
+    {
+        [ScaffoldColumn(false)]
+        public Guid Id { get; set; }
+        [Required, StringLength(100)]
+        public string Name { get; set; }
+
+
+    }
+}
---
+++ WebFormsPractice/Models/Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebFormsPractice.Models
+{
+    public class Task
+    {
+        [ScaffoldColumn(false)]
+        public Guid Id { get; set; }
+        [Required, StringLength(100), Display(Name = "Task")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Display(Name = "Completed")]
+        public bool IsCompleted { get; set; }
+        public Guid CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
